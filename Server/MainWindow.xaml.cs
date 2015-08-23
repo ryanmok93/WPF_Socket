b895335d6e7a5ea80e589d8ca28aab38e5cfd818
Ryan <rision93@hotmail.com>
2015-08-23 19:23:34 +0800

    Modification
    Add and modified some code for server socket.

--- Server/MainWindow.xaml.cs
+++ Server/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Web;
 using System.Net.Sockets;
 using System.Net;
 using System.Threading;
+using Server_socket;
 
 namespace Server
 {
@@ -24,10 +25,15 @@ namespace Server
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private ServerSocket myServerSocket;
+
         public MainWindow()
         {
             InitializeComponent();
             closeServerButton.IsEnabled = false;
+
+            myServerSocket = new ServerSocket();
         }
 
         private void SendButton_Click(object sender, RoutedEventArgs e)
@@ -38,20 +44,45 @@ namespace Server
         private void openServerButton_Click(object sender, RoutedEventArgs e)
         {
             
+            myServerSocket.setPort(Convert.ToInt32(portTextBox.Text));
+            if (myServerSocket.StartServer() == true)
+            {
+
+               openServerButton.IsEnabled = false;
+               closeServerButton.IsEnabled = true;
+                string str = "打开服务器（端口："+portTextBox.Text+"）成功\n";
+                receiveTextBox.AppendText(str);
+            }
+            else
+            {
+                
+            }
+
         }
 
         private void clearButton_Click(object sender, RoutedEventArgs e)
         {
-
+            receiveTextBox.Text = "";
         }
 
         private void closeServerButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if(myServerSocket.StopServer() == true)
+            {
+                closeServerButton.IsEnabled = false;
+                openServerButton.IsEnabled = true;
+            }
+            receiveTextBox.AppendText("服务器已停止\n");
         }
 
         private void exitButton_Click(object sender, RoutedEventArgs e)
         {
+            if(myServerSocket.socketStatus() == true)
+            {
+                myServerSocket.StopServer();
+            }
+            receiveTextBox.Text = "清理缓存...";
+
             this.Close();
         }
     }
---
+++ Server/Server.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Server_socket
+{
+    public class ServerSocket
+    {
+        private String receiveStringMessage = null;
+        private String sendStringMessage = null;
+        private static Socket serverSocket = null;
+        private int myPort=8080; //Set default socket port       
+        IPAddress ip = IPAddress.Parse("127.0.0.1");
+        private static bool statue = false;
+        
+        public bool StartServer()
+        {
+
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.Bind(new IPEndPoint(ip, myPort));
+            serverSocket.Listen(32);
+            
+            statue = true;
+            return true;
+        }
+
+        public void sendMessage()
+        {
+
+        }
+
+        public bool StopServer()
+        {
+            serverSocket.Close();
+            statue = false;
+            return true;
+        }
+
+        public int getPort()
+        {
+            return myPort;
+        }
+
+        public void setPort(int port)
+        {
+            this.myPort = port;
+        }
+        public bool socketStatus()
+        {
+            return statue;
+        }
+        
+    }
+}
